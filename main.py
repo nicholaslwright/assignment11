1b6ec4445d416b4ae8cd21df658e6f3439267178
nicholaslwright <nicholas.wright@kellogg.northwestern.edu>
2021-11-30 11:43:18 -0600

    task 1 and 2

--- main.py
+++ main.py
@@ -13,3 +13,18 @@ xor_data = [
 ]
 nn = NeuralNet(2, 5, 1)
 nn.train(xor_data)
+
+print(nn.evaluate([0.0, 1.0]))
+
+for triple in nn.test_with_expected(xor_data):
+ print(triple)
+
+#Task 2
+print("Task 2:")
+nn = NeuralNet(2, 1, 1)
+nn.train(xor_data)
+
+for triple in nn.test_with_expected(xor_data):
+ print(triple)
+
+# Task 3
