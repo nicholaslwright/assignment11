425c55463de4e11684170cf02dc16259e5f5895d
nicholaslwright <nicholas.wright@kellogg.northwestern.edu>
2021-11-30 11:14:06 -0600

    improve main

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 print("Hello World")
+
+#Task 1
+from neural import NeuralNet
+
+# each row is an (input, output) tuple
+xor_data = [
+ # input output corresponding example
+ ([0.0, 0.0], [0.0]), #[0, 0] => 0
+ ([0.0, 1.0], [1.0]), #[0, 1] => 1
+ ([1.0, 0.0], [1.0]), #[1, 1] => 1
+ ([1.0, 1.0], [0.0]) #[1, 0] => 0
+]
+nn = NeuralNet(2, 5, 1)
+nn.train(xor_data)
